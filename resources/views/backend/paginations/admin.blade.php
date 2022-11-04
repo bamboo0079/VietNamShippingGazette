b64a5656e7d3b1f3030c598c672415f720b85016
fsssaunt <saunt@feelsyncsystem.vn>
2022-11-04 14:09:38 +0700

    change admin paging

--- resources/views/backend/paginations/admin.blade.php
+++ resources/views/backend/paginations/admin.blade.php
@@ -1,3 +1,4 @@
+{{--
 @if ($paginator->lastPage() > 1)
     <nav aria-label="Page navigation example">
         <ul class="pagination" style="justify-content: flex-end;">
@@ -18,4 +19,43 @@
             </li>
         </ul>
     </nav>
+@endif--}}
+
+<?php
+// config
+$link_limit = 12; // maximum number of links
+?>
+@if ($paginator->lastPage() > 1)
+    <section class="pagination__wrapper">
+        <ul class="pagination pagination-sm justify-content-center">
+            <li class="{{ ($paginator->currentPage() == 1) ? ' disabled page-item' : 'page-item' }}">
+                <a class="page-link" href="{{ $paginator->url($paginator->currentPage()-1) }}" aria-label="Previous">
+                    <<
+                </a>
+            </li>
+            @for ($i = 1; $i <= $paginator->lastPage(); $i++)
+                <?php
+                $half_total_links = floor($link_limit / 2);
+                $from = $paginator->currentPage() - $half_total_links;
+                $to = $paginator->currentPage() + $half_total_links;
+                if ($paginator->currentPage() < $half_total_links) {
+                    $to += $half_total_links - $paginator->currentPage();
+                }
+                if ($paginator->lastPage() - $paginator->currentPage() < $half_total_links) {
+                    $from -= $half_total_links - ($paginator->lastPage() - $paginator->currentPage()) - 1;
+                }
+                ?>
+                @if ($from < $i && $i < $to)
+                    <li class="page-item {{ ($paginator->currentPage() == $i) ? ' active' : '' }}">
+                        <a class="page-link"  href="{{ $paginator->url($i) }}">{{ $i }}</a>
+                    </li>
+                @endif
+            @endfor
+            <li class="page-item {{ ($paginator->currentPage() == $paginator->lastPage()) ? ' disabled' : '' }}">
+                <a class="page-link" href="{{ $paginator->url($paginator->currentPage()+1) }}" aria-label="Next">
+                    >>
+                </a>
+            </li>
+        </ul>
+    </section>
 @endif
